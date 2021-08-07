f9f590c3eb4eb709712891a575267222fde046bc
Thayna Francisco <thaynacosta.f@outlook.com>
2021-08-07 11:42:45 -0300

    fixed align

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {AutenticacaoService} from "../../autenticacao/autenticacao.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  usuario= '';
+  senha= '';
+
+  constructor(private authService : AutenticacaoService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+      this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
+        this.router.navigate(['animais'])
+      }, (error)=>{
+        alert("Usuário ou senha invalida")
+        console.log(error)
+        }
+      )
+  }
+}
