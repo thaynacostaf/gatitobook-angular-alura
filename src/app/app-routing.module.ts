02220154fde289ebb9fa907459df7566b3cca608
Thayna Francisco <thayna.francisco@global.ntt>
2021-08-30 17:43:21 -0300

    class number 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,29 @@
-  import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
+import { LoginGuard } from './autenticacao/login.guard';
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch:'full',
-    redirectTo: 'home'
+    pathMatch: 'full',
+    redirectTo: 'home',
   },
   {
     path: 'home',
-    loadChildren:()=> import('./home/home.module').then((m) => m.HomeModule),
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [LoginGuard],
   },
   {
     path: 'animais',
-    loadChildren:()=> import('./animais/animais.module').then((m) => m.AnimaisModule),
-  }
+    loadChildren: () =>
+      import('./animais/animais.module').then((m) => m.AnimaisModule),
+    canLoad: [AutenticacaoGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
