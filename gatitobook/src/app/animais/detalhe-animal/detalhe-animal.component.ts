c36fbbcc229807c111c2f90fdda6e9dbad7a6370
Thayna Francisco <thayna.francisco@global.ntt>
2021-09-22 18:34:20 -0300

    all changes till 22/09

--- gatitobook/src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ gatitobook/src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -23,4 +23,21 @@ export class DetalheAnimalComponent implements OnInit {
     this.animalId = this.activatedRoute.snapshot.params.animalId;
     this.animal$ = this.animaisService.buscaPorID(this.animalId);
   }
+
+  curtir() {
+    this.animaisService.curtir(this.animalId).subscribe((curtida) => {
+      if (curtida) {
+        this.animal$ = this.animaisService.buscaPorID(this.animalId);
+      }
+    });
+  }
+
+  excluir() {
+    this.animaisService.excluiAnimal(this.animalId).subscribe(
+      () => {
+        this.router.navigate(['/animais/']);
+      },
+      (error) => console.log(error)
+    );
+  }
 }
